067dab7f22bf60b755f6472ef282e5c0f8a7531d
PavanKamra96 <35878931+PavanKamra96@users.noreply.github.com>
2018-02-01 18:38:16 -0500

    Create firstCprogram.c

---
+++ Lab1/firstCprogram.c
@@ -0,0 +1,12 @@
+/**
+	Name: Pavan Kumar Kamra
+	Course: BTP100
+	Purpose: First C Program 
+	Compile: gcc firstCprogram.c 
+**/
+
+#include <stdio.h>
+
+main() {
+  printf ("This is my first program. \n");
+}
